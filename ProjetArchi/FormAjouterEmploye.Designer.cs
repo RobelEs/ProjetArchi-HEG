a226404c1954763db8fc6dfa8e95d4964b27460c
betiel-abrh <betiel.abrh@eduge.ch>
2025-03-11 19:44:09 +0100

    Ajoutez des fichiers projet.

---
+++ ProjetArchi/FormAjouterEmploye.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ProjetArchi
+{
+    partial class FormAjouterEmploye
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grpInformation = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbtNom = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpInformation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // grpInformation
+            // 
+            this.grpInformation.Controls.Add(this.label2);
+            this.grpInformation.Controls.Add(this.label1);
+            this.grpInformation.Controls.Add(this.lbtNom);
+            this.grpInformation.Controls.Add(this.textBox3);
+            this.grpInformation.Controls.Add(this.textBox2);
+            this.grpInformation.Controls.Add(this.textBox1);
+            this.grpInformation.Location = new System.Drawing.Point(102, 53);
+            this.grpInformation.Name = "grpInformation";
+            this.grpInformation.Size = new System.Drawing.Size(429, 249);
+            this.grpInformation.TabIndex = 1;
+            this.grpInformation.TabStop = false;
+            this.grpInformation.Text = "Information";
+            this.grpInformation.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(200, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(200, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // lbtNom
+            // 
+            this.lbtNom.AutoSize = true;
+            this.lbtNom.Location = new System.Drawing.Point(23, 66);
+            this.lbtNom.Name = "lbtNom";
+            this.lbtNom.Size = new System.Drawing.Size(36, 16);
+            this.lbtNom.TabIndex = 3;
+            this.lbtNom.Text = "Nom";
+            this.lbtNom.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Prénom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Rôle";
+            // 
+            // FormAjouterEmploye
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpInformation);
+            this.Name = "FormAjouterEmploye";
+            this.Text = "FormAjouterEmploye";
+            this.grpInformation.ResumeLayout(false);
+            this.grpInformation.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox grpInformation;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbtNom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
